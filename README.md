761e0ca7ffa36debd6e47c6575b6a24f5d1048d8
leftieFriele <leftie.friele@gmail.com>
2014-10-31 09:42:02 +0100

    seeded from redesign branch

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-arnfinn
-=======
+# Arnfinn - the FINN.no CSS framework 
 
-The Finn.no front-end CSS framework built on top of OOCSS
+To be added...
+
+## Building
+
+	$ npm install
+
+## Deployment
+
+	$ npm deploy
+
+## Releasing
+
+When releasing you must specify how to bump the version number. Please consult the [semver standard](http://semver.org/) if you're not certain which to choose:
+
+	$ ./node_modules/.bin/gulp release --versionType [patch | minor | major]
+	# Ensure the tags are pushed too
+	$ git push --tags origin <branch name>
+
+## Legacy browser support
+
+	<!--[if IE 9 ]>
+	<link rel="stylesheet" type="text/css" media="screen,projection,handheld" 
+	href="styles/so/ie9.css">
+	<![endif]-->
+	<!--[if IE 8 ]>
+	<link rel="stylesheet" type="text/css" media="screen,projection,handheld" 
+	href="styles/so/ie8.css" />
+	<![endif]-->
+	<!--[if lte IE 8]>
+	<link rel="stylesheet" type="text/css" media="screen,projection,handheld,print" 
+	href="styles/so/ie.css" />
+	<![endif]-->
+
+## Make print outs awesome
+
+	<link rel="stylesheet" type="text/css" media="print" 
+	href="so/print.css">
+# Contributing
+
+## Adding new stuff
+
+1. All new components should reside in separate folders
+1. The component you created should be added to the CSS file which bundles everything in core/components/thirdparty
+
+## Quality checklist
+
+1. You CSS code must work for all screen-sizes
+1. Your widget should work in all browsers
+1. Follow design the [principles of Object Oriented CSS](http://www.smashingmagazine.com/2011/12/12/an-introduction-to-object-oriented-css-oocss/)
---
+++ gulpfile.js
@@ -0,0 +1,124 @@
+var concat = require('gulp-concat');
+var filesize = require('gulp-filesize');
+var gulp = require('gulp');
+var gzip = require('gulp-gzip');
+var maven = require('gulp-maven-deploy');
+var minifyCSS = require('gulp-minify-css');
+var pkg = require('./package');
+var rename = require('gulp-rename');
+var rimraf = require('gulp-rimraf');
+var sourcemaps = require('gulp-sourcemaps');
+var spawn = require('child_process').spawn;
+var tar = require('gulp-tar');
+var util = require('gulp-util');
+
+var distDir = './dist/';
+var buildDir = './build/';
+var destinationDir = distDir + pkg.name + '-' + pkg.version;
+
+var debugMode = false;
+
+gulp.task('clean', function(){
+	return gulp.src([
+		buildDir,
+		distDir
+	], {read: false}).pipe(rimraf({force:true}));
+});
+
+gulp.task('copy-src-toDist', ['clean'], function(){
+	return gulp.src([
+		'./src/**'
+	], {base: 'src/'})
+	.pipe(gulp.dest(destinationDir));
+});
+
+gulp.task('copy-to-build', ['clean'], function(){
+	return gulp.src([
+		'./src/styles/**',
+		'./src/styles/legacy/**'
+		], {base: 'src/styles/'})
+		.pipe(gulp.dest(buildDir));
+});
+
+gulp.task('sass', ['copy-to-build'], function(){
+	// add SASS support....
+});
+
+gulp.task('build-so-css', ['sass'], function(){
+	return gulp.src([
+			buildDir + '/core/core.css',
+			buildDir + '/components/components.css'
+		])
+		.pipe(sourcemaps.init())
+		.pipe(minifyCSS({
+			keepBreaks: true,
+			processImport: true,
+			debug: false
+		}))
+		.pipe(concat('arnfinn.css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(destinationDir))
+		.pipe(sourcemaps.init())
+		.pipe(minifyCSS({
+			keepBreaks: false,
+			processImport: true,
+			debug: true
+		}))
+		.pipe(concat('arnfinn.min.css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(destinationDir))
+});
+
+gulp.task('build-legacy-css', ['sass'], function(){
+	gulp.src(buildDir + '/legacy/**')
+		.pipe(concat('legacy.css'))
+		.pipe(gulp.dest(destinationDir))
+		.pipe(sourcemaps.init())
+		.pipe(minifyCSS({
+			keepBreaks: false,
+			processImport: true,
+			debug: false
+		}))
+		.pipe(concat('legacy.min.css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(destinationDir))
+});
+
+gulp.task('package', ['copy-src-toDist', 'build-so-css', 'build-legacy-css'], function(){
+	return gulp.src([
+			distDir + '/' + pkg.name + '-' + pkg.version + '/**'
+			],
+			{base: distDir + '/' + pkg.name + '-' + pkg.version + '/'}
+		)
+		.pipe(tar(pkg.name + '-' + pkg.version + '.tar'))
+		.pipe(gzip())
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('npm-version', function(done){
+	var versionType = util.env.versionType;
+	spawn('npm', ['version', versionType], { stdio: 'inherit' }).on('close', done);	
+});
+
+gulp.task('npm-publish', ['package'], function(done) {
+  spawn('npm', ['publish', '.'], { stdio: 'inherit' }).on('close', done);
+});
+
+gulp.on('err', function(e) {
+  console.log(e.err.stack);
+});
+
+gulp.task('release', [
+	'npm-version',
+	'copy-src-toDist',
+	'build-so-css',
+	'build-legacy-css',
+	'package',
+	'npm-publish'
+]);
+gulp.task('default', [
+	'sass',
+	'build-so-css',
+	'build-legacy-css',
+	'package'
+]);
